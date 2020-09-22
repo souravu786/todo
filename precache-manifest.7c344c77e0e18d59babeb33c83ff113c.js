b973362244d70afaee81dafe11a73a3cb9824ec4
Saurav <souravu786@gmail.com>
2020-09-22 11:29:25 +0530

    Updates

---
+++ precache-manifest.7c344c77e0e18d59babeb33c83ff113c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ec32b84c08b82cde3af7cba70d5a84",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b481a1fd7839fd9f135f",
+    "url": "/todo/static/css/main.56fb0b49.chunk.css"
+  },
+  {
+    "revision": "90a16237435dd5dcbc58",
+    "url": "/todo/static/js/2.cb0aadce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.cb0aadce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b481a1fd7839fd9f135f",
+    "url": "/todo/static/js/main.a2f93530.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
